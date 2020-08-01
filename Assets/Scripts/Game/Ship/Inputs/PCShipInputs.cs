7fef8e508625f980aa42cc91ea287dd3429cc843
Lyrapuff <lyrapuff@gmail.com>
2020-08-01 18:16:38 +0300

    PC Inputs

--- Assets/Scripts/Game/Ship/Inputs/PCShipInputs.cs
+++ Assets/Scripts/Game/Ship/Inputs/PCShipInputs.cs
@@ -7,14 +7,14 @@ namespace SpaceShooter.Game.Ship.Inputs
     {
         public Action OnShoot { get; set; }
         public Action<Vector3> OnMove { get; set; }
-        public Vector2 Direction { get; private set; }
 
         private void Update()
         {
             float horizontal = Input.GetAxisRaw("Horizontal");
             float vertical = Input.GetAxisRaw("Vertical");
             
-            Direction = new Vector2(horizontal, vertical);
+            Vector3 direction = new Vector3(horizontal, vertical, 0f);
+            OnMove?.Invoke(direction);
 
             if (Input.GetButton("Shoot"))
             {
